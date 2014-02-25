9ff26349c9a7850dcba3d90bb1ccec0760e278d6
Terrizen <geurdsts@clarkson.edu>
2014-02-25 16:09:36 -0500

    Steps.txt
    So the point of this lab was to create an Initial Shader for our previous lab.
    Step 1 - add the #ifndef, the #define, and the #includes that are needed for the Shader.
    
    Next step was to create a struct that included the GL vertex and fragment Shader variable as well as adding the a variable for the file name. once we do that we need to create the the shaders for the program.
    
    I decided to use a vector it initialize the list of shaders and used a while loop to loop all the shaders into the list. after that i created a program that linked all the shaders in the list. After that i then installed the program as part of the current rendering state. 
    
    The next thing i did was use make a function that loads the shader from the vertex. I opened the fragment shader file and checked to see if the file opened. once i opened the file i read the file into the shader. I also checked to see if the file was empty. once i read in the file i closed it.
    
    The next step was creating a "create shader" function to creat the shader. this function will create the shader base on either the vertex shader or the fragment shader. It will then load the the source code of the file into the shader. Next it will compile the object, check the compile status, and will return the compile status. The program will then check to see if the shader compiled.
    
    If it didn't compile we need to know where the shader bugged so we can debug it. we can make an array to store the memory of the bugged info that needs to be debugged. with this array we can determine which shaders bugged during the compile. We can also create a print command to print the information that we need to debug the shader. after that we can delete the bugged memory and return the shader.
    
    the next function we need to create is one that will link the shaders. first we are going to need to attach the shaders to this program. we can call the list vector and use the "glAttachShader" to attach the program. next we need to bind the local of the "position and color" attributes to the program.Once we do that we can link this program with our program.
    
    We need to check to see if every shader linked to the program. to do this we need to check for bugs like we did when we compiled. if the program didn't link to our program we then need to create an array to store our bugged info. once we stored the info we can then use a print command to let us know that our link failed. after that we can delete the debugged memory. once we delete the bad memory we can then use a for loop to delete the memory for the list of shaders and return the program.

---
+++ InitShaders.h
@@ -0,0 +1,128 @@
+#ifndef INITSHADERS_H_
+#define INITSHADERS_H_
+
+//#include "SDL2/SDL.h"
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "GL/gl.h"
+#include "GL/glu.h"
+
+#include <vector>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+GLuint createShader(GLenum type, const GLchar* shade_source);
+const GLchar* inputShader(const char* file_name);
+GLuint createProgram(const vector<GLuint> shade_list);
+
+typedef struct{
+  GLenum type;// GL_VERTEX_SHADER or GL_FRAGMENT_SHADER
+  const char* file_name;
+} ShaderInfo;
+
+//create the shaders for the program
+void initShaders(ShaderInfo* shaders){
+  
+  ShaderInfo* shade=shaders;
+  vector<GLuint> Shade_list;
+  
+  while(shade->type != GL_NONE){
+    Shade_list.push_back(createShader(shade->type,inputShader(shade->file_name)));
+  }
+  
+  GLuint program=createProgram(Shade_list);//creates the program linking all the shaders
+  
+  glUseProgram(program);//installs a program object as part of current rendering state
+}
+
+
+const GLchar* inputShader(const char* file_name){
+
+  FILE* fshade = fopen(file_name, "rb");//opens file
+  
+  if(!fshade){
+    fprintf(stderr,"unable to open file '%s'\n",file_name);
+    return NULL;
+  }
+  
+  fseek(fshade, 0, SEEK_END);
+  long file_size=ftell(fshade);
+  fseek(fshade, 0, SEEK_SET);
+  
+  
+  GLchar* shading_source= new GLchar[file_size+1];//
+  fread(shading_source, 1, file_size, fshade);
+  
+  
+  if(ftell(fshade) == 0){
+    fprintf(stderr, "File '%s' is empty.\n",file_name);
+    return NULL;
+  }
+
+  fclose(fshade);
+  
+  shading_source[filesize] = 0;
+  
+  return const_cast<const GLchar*>(shading_source);
+  
+}
+
+//this function creates the shader
+GLuint createShader(GLenum type, const GLchar* shade_source){
+  
+  GLuint shader = glCreateShader(type);
+  glShaderSource(shader, 1, &shadeSource, NULL);
+  
+  glCompileShader(shader);
+  
+  GLint compileStatus;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compileStatus);
+  
+  if(!compileStatus){
+    GLint log_size;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_Size);
+    
+    GLchar* info_log = new GLchar[log_Size+1];
+    glGetShaderInfoLog(shader,log_size,&log_size,info_log);
+    
+    const char *shade_info= NULL;
+    switch(type){
+      case GL_VERTEX_SHADER: shade_info = "vertex"; break;
+      case GL_GEOMETRY_SHADER_EXT: shade_info = "geometric"; break;
+      case GL_FRAGMENT_SHADER: shade_info = "fragment"; break;
+    }
+    fprintf(stderr,"\nCompile failure in %u shader: %s\n Error message:\n%s\n",type,shadeInfo,infoLog);//prints information need to debug shaders
+    delete[] info_log;
+  }
+  return shader;
+}
+
+GLuint createProgram(const vector<GLuint> shade_list){
+
+  GLuint program = glCreateProgram();
+  
+  for(GLuint i=0;i<shade_list.size();i++){glAttachShader(program,shade_list[i]);}
+  
+  glBindAttribLocation(program, 0, "in_position");
+  glBindAttribLocation(program, 0, "in_color");
+  glLinkProgram(program);
+  
+  GLint link_status;
+  glGetProgramiv(program, GL_LINK_STATUS, &link_status);
+  
+  if(!link_status){
+    GLint log_size;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_size);
+    
+    GLchar *info_log = new GLchar[log_size+1];
+    glGetProgramInfoLog(program,log_size,&log_size,info_log);
+    
+    fprintf(stderr,"\nShader linking failed: %s\n",info_log);
+    delete[] info_log;
+    
+    for(GLuint i=0;i<shade_list.size();i++){glDeleteShader(shade_list[i]);}
+  }
+  return program;
+}
+#endif
